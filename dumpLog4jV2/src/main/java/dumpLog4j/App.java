9cf6e3936bdabf89acbc0e331715a6b304e9db2e
Mark Gavrilman <markgav19@gmail.com>
2022-06-17 12:55:54 +0300

    initial_commit updated

--- dumpLog4jV2/src/main/java/dumpLog4j/App.java
+++ dumpLog4jV2/src/main/java/dumpLog4j/App.java
@@ -20,37 +20,35 @@ import org.apache.logging.log4j.core.config.properties.PropertiesConfigurationBu
  * Hello world!
  *
  */
-public class App 
-{
-	public static  Logger logger = LogManager.getLogger(App.class);
-    public static void main( String[] args )
-    {
-        String loadFrom="resourceBundle";
-        if(loadFrom.equals("properties")) {
-        	loadFromProperties();
-        }
-        if(loadFrom.equals("xml")) {
-        	loadFromXml();
-        }
-        if(loadFrom.equals("propertiesObject")) {
-        	loadFromPropertiesObject();
-        }
-        if(loadFrom.equals("resourceBundle")) {
-        	loadFromResourceBundle();
-        }
-        
-        //Logger logger = LogManager.getRootLogger();
+public class App {
+	public static Logger logger = LogManager.getLogger(App.class);
 
-        logger.error("This is error message comming from "+ loadFrom);
-        logger.info("This is info message comming from "+ loadFrom);
-        logger.trace("This is trace message comming from "+ loadFrom);
-        
-        
-        
-    }
-    public static void loadFromProperties() {
-    	String propertiesFile = "./resources/log4j2.properties";  
-    	ConfigurationSource source;
+	public static void main(String[] args) {
+		String loadFrom = "resourceBundle";
+		if (loadFrom.equals("properties")) {
+			loadFromProperties();
+		}
+		if (loadFrom.equals("xml")) {
+			loadFromXml();
+		}
+		if (loadFrom.equals("propertiesObject")) {
+			loadFromPropertiesObject();
+		}
+		if (loadFrom.equals("resourceBundle")) {
+			loadFromResourceBundle();
+		}
+
+		// Logger logger = LogManager.getRootLogger();
+
+		logger.error("This is error message comming from " + loadFrom);
+		logger.info("This is info message comming from " + loadFrom);
+		logger.trace("This is trace message comming from " + loadFrom);
+
+	}
+
+	public static void loadFromProperties() {
+		String propertiesFile = "./resources/log4j2.properties";
+		ConfigurationSource source;
 		try {
 			source = new ConfigurationSource(new FileInputStream(propertiesFile), new File(propertiesFile));
 			Configurator.initialize(null, source);
@@ -58,76 +56,72 @@ public class App
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-public static void loadFromXml() {
-	String logConfigurationFile="./resources/log4j2.xml";
-    System.out.println( "Hello World!" );
-    InputStream inputStream = null;
-
-    ConfigurationSource source=null;
-	try {
-		inputStream = new FileInputStream(logConfigurationFile);
-		source = new ConfigurationSource(inputStream);
-		Configurator.initialize(null, source);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	}
-    }
 
-public static void loadFromPropertiesObject() {
-	  Properties properties = new Properties();
+	public static void loadFromXml() {
+		String logConfigurationFile = "./resources/log4j2.xml";
+		System.out.println("Hello World!");
+		InputStream inputStream = null;
 
-      properties.setProperty("status", "TRACE");
+		ConfigurationSource source = null;
+		try {
+			inputStream = new FileInputStream(logConfigurationFile);
+			source = new ConfigurationSource(inputStream);
+			Configurator.initialize(null, source);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-      properties.setProperty("appenders", "CONSOLE");
+	public static void loadFromPropertiesObject() {
+		Properties properties = new Properties();
 
-      properties.setProperty("appender.CONSOLE.name", "ConsoleAppender");
-      properties.setProperty("appender.CONSOLE.type", "Console");
-      properties.setProperty("appender.CONSOLE.layout.type", "PatternLayout");
-      properties.setProperty("appender.CONSOLE.layout.pattern", "%r [%t] %p %c %notEmpty{%x }- %m%n");
+		properties.setProperty("status", "TRACE");
 
-      properties.setProperty("rootLogger.level", "ALL");
-      properties.setProperty("rootLogger.appenderRefs", "theConsoleRef");
-      properties.setProperty("rootLogger.appenderRef.theConsoleRef.ref", "ConsoleAppender");
+		properties.setProperty("appenders", "CONSOLE");
 
-      PropertiesConfigurationBuilder pcb = new PropertiesConfigurationBuilder();
+		properties.setProperty("appender.CONSOLE.name", "ConsoleAppender");
+		properties.setProperty("appender.CONSOLE.type", "Console");
+		properties.setProperty("appender.CONSOLE.layout.type", "PatternLayout");
+		properties.setProperty("appender.CONSOLE.layout.pattern", "%r [%t] %p %c %notEmpty{%x }- %m%n");
 
-      // Q1: Is it correct to call setConfigurationSource with 'null'?
-      pcb.setConfigurationSource(null).setRootProperties(properties);
-      PropertiesConfiguration config = pcb.build();
-      Configurator.initialize(config);
-}
+		properties.setProperty("rootLogger.level", "ALL");
+		properties.setProperty("rootLogger.appenderRefs", "theConsoleRef");
+		properties.setProperty("rootLogger.appenderRef.theConsoleRef.ref", "ConsoleAppender");
 
-public static void loadFromResourceBundle() {
-	 // create a new ResourceBundle with default locale
-    ResourceBundle bundle = ResourceBundle.getBundle("log4j2");
+		PropertiesConfigurationBuilder pcb = new PropertiesConfigurationBuilder();
 
-    // print the text assigned to key "hello"
-   // System.out.println("" + bundle.getString("hello"));
-    
-    Properties properties = convertResourceBundleToProperties(bundle);
-    PropertiesConfigurationBuilder pcb = new PropertiesConfigurationBuilder();
+		// Q1: Is it correct to call setConfigurationSource with 'null'?
+		pcb.setConfigurationSource(null).setRootProperties(properties);
+		PropertiesConfiguration config = pcb.build();
+		Configurator.initialize(config);
+	}
 
-    // Q1: Is it correct to call setConfigurationSource with 'null'?
-    pcb.setConfigurationSource(null).setRootProperties(properties);
-    PropertiesConfiguration config = pcb.build();
-    Configurator.initialize(config);
-}
+	public static void loadFromResourceBundle() {
+		
+		ResourceBundle bundle = ResourceBundle.getBundle("log4j2");
 
+		Properties properties = convertResourceBundleToProperties(bundle);
+		PropertiesConfigurationBuilder pcb = new PropertiesConfigurationBuilder();
+		
+		pcb.setConfigurationSource(null).setRootProperties(properties);
+		PropertiesConfiguration config = pcb.build();
+		Configurator.initialize(config);
+	}
 
-public static Properties convertResourceBundleToProperties(ResourceBundle resource) {
-    Enumeration iter;
-    if (resource == null) {
-        return null;
-    }
-    Properties props = new Properties();
-    iter = resource.getKeys();
-    while (iter.hasMoreElements()) {
-        String key = (String) iter.nextElement();
-        String value = resource.getString(key);
-        props.put(key, value);
-    }
-    return props;
-}
+	public static Properties convertResourceBundleToProperties(ResourceBundle resource) {
+		Enumeration iter;
+		if (resource == null) {
+			return null;
+		}
+		Properties props = new Properties();
+		iter = resource.getKeys();
+		while (iter.hasMoreElements()) {
+			String key = (String) iter.nextElement();
+			String value = resource.getString(key);
+			props.put(key, value);
+		}
+		return props;
+	}
 }
